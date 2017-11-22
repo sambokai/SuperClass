0b2b12487f8579917d656284163230e97ea9c76e
sambokai <7689616+sambokai@users.noreply.github.com>
2017-11-22 17:59:13 +0100

    Refactor: wrap instantiation tests in their own nested scope

--- src/test/scala/challenges/superlist/SuperListSpec.scala
+++ src/test/scala/challenges/superlist/SuperListSpec.scala
@@ -5,51 +5,54 @@ import org.scalatest._
 
 class SuperListSpec extends WordSpec {
   "A SuperList" should {
-    "wrap a non empty list on instantiation" in {
-      // given
-      val list = List(1, 2, 3)
+    "on instantiation" should {
+      "wrap a non empty list on instantiation" in {
+        // given
+        val list = List(1, 2, 3)
 
-      // when
-      val result = new SuperList(list)
+        // when
+        val result = new SuperList(list)
 
-      // then
-      result.list shouldBe list
-    }
-
-    "be able to wrap an empty list" in {
-      // given
-      val list = List.empty
-
-      // when
-      val result = new SuperList(list)
-
-      // then
-      result.list shouldBe list
-    }
-
-    "throw an exception if passing a null value" in {
-      intercept[IllegalArgumentException] {
-        new SuperList(null)
+        // then
+        result.list shouldBe list
       }
-    }
 
-    "wrap int parameters" in {
-      //when
-      val result = new SuperList(1, 2, 3)
+      "be able to wrap an empty list" in {
+        // given
+        val list = List.empty
 
-      //then
-      result.list shouldBe List(1, 2, 3)
-    }
+        // when
+        val result = new SuperList(list)
 
-    "instantiate wrapper without 'new' keyword and a List[Int] as parameter" in {
-      val list = List(1, 2, 3)
-      val result = SuperList(list)
-      result.list shouldBe list
-    }
+        // then
+        result.list shouldBe list
+      }
 
-    "instantiate wrapper without 'new' keyword and class parameters" in {
-      val result = SuperList(1, 2, 3)
-      result.list shouldBe List(1, 2, 3)
+      "throw an exception if passing a null value" in {
+        intercept[IllegalArgumentException] {
+          new SuperList(null)
+        }
+      }
+
+      "wrap int parameters" in {
+        //when
+        val result = new SuperList(1, 2, 3)
+
+        //then
+        result.list shouldBe List(1, 2, 3)
+      }
+
+      "instantiate wrapper without 'new' keyword and a List[Int] as parameter" in {
+        val list = List(1, 2, 3)
+        val result = SuperList(list)
+        result.list shouldBe list
+      }
+
+      "instantiate wrapper without 'new' keyword and class parameters" in {
+        val result = SuperList(1, 2, 3)
+        println(result)
+        result.list shouldBe List(1, 2, 3)
+      }
     }
 
 
